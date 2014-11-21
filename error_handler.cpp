37a787b1d1a33d4aaa92f21ffc8f33da52f7585d
Adib Hasan <Neehan@users.noreply.github.com>
2014-11-21 11:58:37 +0600

    Create error_handler.cpp

---
+++ error_handler.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
