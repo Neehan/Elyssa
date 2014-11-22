d021cedd3b796f9b843d0bd758ba8a9c21b8f455
Adib Hasan <Neehan@users.noreply.github.com>
2014-11-22 11:40:10 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,22 +18,87 @@ using namespace std;
 
 #define PI 3.1415927
 
+map<string,double> knowns1,knowns2,knowns3;
 struct Equation{
     string lhs,rhs;
     vector<string> val;
     int prec;
 };
-/**m
-2
-m1 = 4
-u1 = 12
-2
-m2 = 6
-u2 = -4
-y
-1
-v1 = v2 v1 v2
-v1**/
+
+void trig(string mystr, double nval, int no){
+    string pemp=mystr.substr(0,3);
+    double mval;
+    if(no==1){
+        if(pemp=="sin"){
+            mval=asin(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns1[pemp]=mval*180/PI;
+            knowns1["cos"+pemp]=cos(mval);
+            knowns1["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="cos"){
+            mval=acos(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns1[pemp]=mval*180.0/PI;
+            knowns1["sin"+pemp]=sin(mval);
+            knowns1["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="tan"){
+            mval=atan(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns1[pemp]=mval*180.0/PI;
+            knowns1["cos"+pemp]=cos(mval);
+            knowns1["sin"+pemp]=sin(mval);
+        }
+    }
+    else if(no==2){
+        if(pemp=="sin"){
+            mval=asin(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns2[pemp]=mval*180/PI;
+            knowns2["cos"+pemp]=cos(mval);
+            knowns2["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="cos"){
+            mval=acos(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns2[pemp]=mval*180.0/PI;
+            knowns2["sin"+pemp]=sin(mval);
+            knowns2["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="tan"){
+            mval=atan(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns2[pemp]=mval*180.0/PI;
+            knowns2["cos"+pemp]=cos(mval);
+            knowns2["sin"+pemp]=sin(mval);
+        }
+    }
+    else if(no==3){
+        if(pemp=="sin"){
+            mval=asin(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns3[pemp]=mval*180/PI;
+            knowns3["cos"+pemp]=cos(mval);
+            knowns3["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="cos"){
+            mval=acos(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns3[pemp]=mval*180.0/PI;
+            knowns3["sin"+pemp]=sin(mval);
+            knowns3["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="tan"){
+            mval=atan(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns3[pemp]=mval*180.0/PI;
+            knowns3["cos"+pemp]=cos(mval);
+            knowns3["sin"+pemp]=sin(mval);
+        }
+    }
+}
+
 int main()
 {
     xx:
@@ -41,7 +106,7 @@ int main()
     register int i,j,temp,k=0,prec=-1;
     double nval,mval;
     ifstream fin("con_eq.txt");
-    cout << "(S)ingle object\t (M)ultiple object\n";
+    cout << "(S)ingle object\t (M)ultiple objects\n";
     cin >> b;
     if(b=='s'||b=='S'){
         map<string,double> knowns;
@@ -130,7 +195,6 @@ int main()
         if(knowns.find(unknown)!=knowns.end())cout << knowns[unknown] << endl;
     }
     else{
-        map<string,double> knowns1,knowns2,knowns3;
         map<string,double>::iterator myit;
         string unknown="x",pemp, hmm;
         string val1,val2;
@@ -214,6 +278,23 @@ int main()
             knowns[pemp]=nval;
             i++;
         }**/
+
+        /**
+m
+4
+d1 = 1.2
+A1 = 3e-4
+delta_Theta1 = 100
+K1 = 385
+3
+l_f2 = 336000
+m2 = 10e-3
+/fusion2 = 0
+y
+1
+Q1 = delta_Q2 Q1 delta_Q2
+t1
+        **/
         ifstream rin("eq.txt");
         while(rin >> a.lhs){
             a.val.clear();
@@ -250,52 +331,10 @@ int main()
                 nval=solve(myeq[i].lhs,myeq[i].rhs,knowns1,myeq[i].val[j]);
                 myeq[i].val[j]+="1";
                 knowns3[myeq[i].val[j]]=nval;
-                pemp=myeq[i].val[j].substr(0,3);
-                if(pemp=="sin"){
-                    mval=asin(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180/PI;
-                    knowns3["cos"+pemp]=cos(mval);
-                    knowns3["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="cos"){
-                    mval=acos(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180.0/PI;
-                    knowns3["sin"+pemp]=sin(mval);
-                    knowns3["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="tan"){
-                    mval=atan(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180.0/PI;
-                    knowns3["cos"+pemp]=cos(mval);
-                    knowns3["sin"+pemp]=sin(mval);
-                }
+                trig(myeq[i].val[j],nval,3);
                 myeq[i].val[j].erase(myeq[i].val[j].length()-1);
                 knowns1[myeq[i].val[j]]=nval;
-                pemp=myeq[i].val[j].substr(0,3);
-                if(pemp=="sin"){
-                    mval=asin(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns1[pemp]=mval*180/PI;
-                    knowns1["cos"+pemp]=cos(mval);
-                    knowns1["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="cos"){
-                    mval=acos(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns1[pemp]=mval*180.0/PI;
-                    knowns1["sin"+pemp]=sin(mval);
-                    knowns1["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="tan"){
-                    mval=atan(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns1[pemp]=mval*180.0/PI;
-                    knowns1["cos"+pemp]=cos(mval);
-                    knowns1["sin"+pemp]=sin(mval);
-                }
+                trig(myeq[i].val[j],nval,1);
                 i=0;
             }
             else i++;
@@ -315,52 +354,10 @@ int main()
                 nval=solve(myeq[i].lhs,myeq[i].rhs,knowns2,myeq[i].val[j]);
                 myeq[i].val[j]+="2";
                 knowns3[myeq[i].val[j]]=nval;
-                pemp=myeq[i].val[j].substr(0,3);
-                if(pemp=="sin"){
-                    mval=asin(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180/PI;
-                    knowns3["cos"+pemp]=cos(mval);
-                    knowns3["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="cos"){
-                    mval=acos(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180.0/PI;
-                    knowns3["sin"+pemp]=sin(mval);
-                    knowns3["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="tan"){
-                    mval=atan(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180.0/PI;
-                    knowns3["cos"+pemp]=cos(mval);
-                    knowns3["sin"+pemp]=sin(mval);
-                }
+                trig(myeq[i].val[j],nval,3);
                 myeq[i].val[j].erase(myeq[i].val[j].length()-1);
                 knowns2[myeq[i].val[j]]=nval;
-                pemp=myeq[i].val[j].substr(0,3);
-                if(pemp=="sin"){
-                    mval=asin(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns2[pemp]=mval*180/PI;
-                    knowns2["cos"+pemp]=cos(mval);
-                    knowns2["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="cos"){
-                    mval=acos(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns2[pemp]=mval*180.0/PI;
-                    knowns2["sin"+pemp]=sin(mval);
-                    knowns2["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="tan"){
-                    mval=atan(nval);
-                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                    knowns2[pemp]=mval*180.0/PI;
-                    knowns2["cos"+pemp]=cos(mval);
-                    knowns2["sin"+pemp]=sin(mval);
-                }
+                trig(myeq[i].val[j],nval,2);
                 i=0;
             }
             else i++;
@@ -379,28 +376,7 @@ int main()
             if(counter==1){
                 nval=solve(alge[i].lhs,alge[i].rhs,knowns3,alge[i].val[j]);
                 knowns3[alge[i].val[j]]=nval;
-                pemp=alge[i].val[j].substr(0,3);
-                if(pemp=="sin"){
-                    mval=asin(nval);
-                    pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180/PI;
-                    knowns3["cos"+pemp]=cos(mval);
-                    knowns3["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="cos"){
-                    mval=acos(nval);
-                    pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180.0/PI;
-                    knowns3["sin"+pemp]=sin(mval);
-                    knowns3["tan"+pemp]=tan(mval);
-                }
-                else if(pemp=="tan"){
-                    mval=atan(nval);
-                    pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                    knowns3[pemp]=mval*180.0/PI;
-                    knowns3["cos"+pemp]=cos(mval);
-                    knowns3["sin"+pemp]=sin(mval);
-                }
+                trig(alge[i].val[j],nval,3);
                 i=0;
             }
             else i++;
@@ -418,113 +394,75 @@ int main()
                 if(counter==2){
                     nval=solve(alge[0].lhs,alge[0].rhs,alge[i].lhs,alge[i].rhs,knowns3,alge[0].val[0], alge[0].val[1]);
                     knowns3[alge[0].val[1]]=nval;
-                    i=0;
-                    j=1;
-                    pemp=alge[i].val[j].substr(0,3);
-                    if(pemp=="sin"){
-                        mval=asin(nval);
-                        pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                        knowns3[pemp]=mval*180/PI;
-                        knowns3["cos"+pemp]=cos(mval);
-                        knowns3["tan"+pemp]=tan(mval);
-                    }
-                    else if(pemp=="cos"){
-                        mval=acos(nval);
-                        pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                        knowns3[pemp]=mval*180.0/PI;
-                        knowns3["sin"+pemp]=sin(mval);
-                        knowns3["tan"+pemp]=tan(mval);
-                    }
-                    else if(pemp=="tan"){
-                        mval=atan(nval);
-                        pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                        knowns3[pemp]=mval*180.0/PI;
-                        knowns3["cos"+pemp]=cos(mval);
-                        knowns3["sin"+pemp]=sin(mval);
-                    }
+                    trig(alge[0].val[1],nval,3);
+                    pemp=alge[0].val[1];
                     nval=solve(alge[0].lhs,alge[0].rhs,knowns3,alge[0].val[0]);
                     knowns3[alge[0].val[0]]=nval;
-                    i=0;
-                    j=0;
-                    pemp=alge[i].val[j].substr(0,3);
-                    if(pemp=="sin"){
-                        mval=asin(nval);
-                        pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                        knowns3[pemp]=mval*180/PI;
-                        knowns3["cos"+pemp]=cos(mval);
-                        knowns3["tan"+pemp]=tan(mval);
-                    }
-                    else if(pemp=="cos"){
-                        mval=acos(nval);
-                        pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                        knowns3[pemp]=mval*180.0/PI;
-                        knowns3["sin"+pemp]=sin(mval);
-                        knowns3["tan"+pemp]=tan(mval);
-                    }
-                    else if(pemp=="tan"){
-                        mval=atan(nval);
-                        pemp=alge[i].val[j].substr(3,alge[i].val[j].length()-3);
-                        knowns3[pemp]=mval*180.0/PI;
-                        knowns3["cos"+pemp]=cos(mval);
-                        knowns3["sin"+pemp]=sin(mval);
-                    }
-                    break;
+                    trig(alge[0].val[0],nval,3);
                 }
                 else i++;
             }
         }
-        int indice[2];
+        for(myit=knowns3.begin();myit!=knowns3.end();myit++){
+            pemp=myit->first;
+            if(pemp[pemp.length()-1]=='1'){
+                pemp.erase(pemp.length()-1);
+                knowns1[pemp]=myit->second;
+                trig(pemp,nval,1);
+            }
+            else if(pemp[pemp.length()-1]=='2'){
+                pemp.erase(pemp.length()-1);
+                knowns2[pemp]=myit->second;
+                trig(pemp,nval,2);
+            }
+        }
+        i=0;
+        while(i<myeq.size()){
+            int counter=0;
+            dfltind.clear();
+            for(j=0;j<myeq[i].val.size();j++){
+                if(knowns1.find(myeq[i].val[j])==knowns1.end()){
+                    counter++;
+                    temp=j;
+                }
+            }
+            j=temp;
+            if(counter==1){
+                nval=solve(myeq[i].lhs,myeq[i].rhs,knowns1,myeq[i].val[j]);
+                myeq[i].val[j]+="1";
+                knowns3[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,3);
+                myeq[i].val[j].erase(myeq[i].val[j].length()-1);
+                knowns1[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,1);
+                i=0;
+            }
+            else i++;
+        }
+        i=0;
+        while(i<myeq.size()){
+            int counter=0;
+            dfltind.clear();
+            for(j=0;j<myeq[i].val.size();j++){
+                if(knowns2.find(myeq[i].val[j])==knowns2.end()){
+                    counter++;
+                    temp=j;
+                }
+            }
+            j=temp;
+            if(counter==1){
+                nval=solve(myeq[i].lhs,myeq[i].rhs,knowns2,myeq[i].val[j]);
+                myeq[i].val[j]+="2";
+                knowns3[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,3);
+                myeq[i].val[j].erase(myeq[i].val[j].length()-1);
+                knowns2[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,2);
+                i=0;
+            }
+            else i++;
+        }
         if(knowns3.find(unknown)!=knowns3.end())cout << knowns3[unknown] << endl;
-        /**else{
-            unknown+=b;
-            while(i<myeq.size()){
-                int counter=0;
-                dfltind.clear();
-                for(j=0;j<myeq[i].val.size();j++){
-                    if(knowns1.find(myeq[i].val[j])==knowns2.end()){
-                        for(k=0;k<alge.size();k++){
-                            if(alge[k].val[0]==myeq[i].val[j]||alge[k].val[1]==myeq[i].val[j]){
-                                indice[counter]=j;
-                                counter++;
-                                break;
-                            }
-                        }
-                        temp=j;
-                    }
-                }
-                j=temp;
-                if(counter==2){
-                    nval=solve(myeq[i].lhs,myeq[i].rhs,knowns2,myeq[i].val[j]);
-                    knowns2[myeq[i].val[j]]=nval;
-                    pemp=myeq[i].val[j].substr(0,3);
-                    //cout << myeq[i].lhs << "=" << myeq[i].rhs << " " << myeq[i].val[j] << " " << nval << endl;
-                    if(pemp=="sin"){
-                        mval=asin(nval);
-                        pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                        knowns2[pemp]=mval*180/PI;
-                        knowns2["cos"+pemp]=cos(mval);
-                        knowns2["tan"+pemp]=tan(mval);
-                    }
-                    else if(pemp=="cos"){
-                        mval=acos(nval);
-                        pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                        knowns2[pemp]=mval*180.0/PI;
-                        knowns2["sin"+pemp]=sin(mval);
-                        knowns2["tan"+pemp]=tan(mval);
-                    }
-                    else if(pemp=="tan"){
-                        mval=atan(nval);
-                        pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
-                        knowns2[pemp]=mval*180.0/PI;
-                        knowns2["cos"+pemp]=cos(mval);
-                        knowns2["sin"+pemp]=sin(mval);
-                    }
-                    i=0;
-                }
-                else i++;
-            }
-        }
-        **/
     }
     cout << "(N)ew\t(Q)uit\n";
     cin >> b;
