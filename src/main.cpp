a8316433431f8e62b5f05e83bd39bbb365aeee1b
Adib Hasan <Neehan@users.noreply.github.com>
2014-12-20 22:26:11 +0600

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,510 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include "etc.cpp"
+#include "include/fraction.h"
+#include "include/parser.h"
+#include "include/eqsolver.h"
+#include "error_handler.cpp"
+
+using namespace std;
+
+#define PI 3.1415927
+
+map<string,double> knowns1,knowns2,knowns3;
+
+void trig(string mystr, double nval, int no){
+    string pemp=mystr.substr(0,3);
+    double mval;
+    if(no==1){
+        if(pemp=="sin"){
+            mval=asin(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns1[pemp]=mval*180/PI;
+            knowns1["cos"+pemp]=cos(mval);
+            knowns1["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="cos"){
+            mval=acos(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns1[pemp]=mval*180.0/PI;
+            knowns1["sin"+pemp]=sin(mval);
+            knowns1["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="tan"){
+            mval=atan(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns1[pemp]=mval*180.0/PI;
+            knowns1["cos"+pemp]=cos(mval);
+            knowns1["sin"+pemp]=sin(mval);
+        }
+    }
+    else if(no==2){
+        if(pemp=="sin"){
+            mval=asin(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns2[pemp]=mval*180/PI;
+            knowns2["cos"+pemp]=cos(mval);
+            knowns2["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="cos"){
+            mval=acos(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns2[pemp]=mval*180.0/PI;
+            knowns2["sin"+pemp]=sin(mval);
+            knowns2["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="tan"){
+            mval=atan(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns2[pemp]=mval*180.0/PI;
+            knowns2["cos"+pemp]=cos(mval);
+            knowns2["sin"+pemp]=sin(mval);
+        }
+    }
+    else if(no==3){
+        if(pemp=="sin"){
+            mval=asin(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns3[pemp]=mval*180/PI;
+            knowns3["cos"+pemp]=cos(mval);
+            knowns3["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="cos"){
+            mval=acos(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns3[pemp]=mval*180.0/PI;
+            knowns3["sin"+pemp]=sin(mval);
+            knowns3["tan"+pemp]=tan(mval);
+        }
+        else if(pemp=="tan"){
+            mval=atan(nval);
+            pemp=mystr.substr(3,mystr.length()-3);
+            knowns3[pemp]=mval*180.0/PI;
+            knowns3["cos"+pemp]=cos(mval);
+            knowns3["sin"+pemp]=sin(mval);
+        }
+    }
+}
+
+int main()
+{
+    xx:
+    char b;
+    register int i,j,temp,k=0,prec=-1;
+    double nval,mval;
+    ifstream fin("config/con-eq.ini");
+    cout << "(S)ingle object\t (M)ultiple objects\n";
+    cin >> b;
+    if(b=='s'||b=='S'){
+        map<string,double> knowns;
+        map<string,double>::iterator myit;
+        string unknown="x",pemp, hmm;
+        string val1,val2;
+        vector<Equation> myeq;
+        Equation a;
+        cout << "Number of knowns=";
+        cin >> temp;
+        cout << "Write them one at a line\n";
+        for(i=0;i<temp;i++){
+            cin >> pemp;
+            cin >> hmm >> nval;
+            knowns[pemp]=nval;
+            if(pemp.find("theta")!=string::npos){
+                nval*=(PI/180);
+                knowns[pemp]=nval;
+                knowns["sin"+pemp]=sin(nval);
+                knowns["cos"+pemp]=cos(nval);
+                knowns["tan"+pemp]=tan(nval);
+
+            }
+        }
+        cout << "What to find=";
+        cin >> unknown;
+        /**ifstream tin("const.txt");
+        i=0;
+        while(tin >> pemp){
+            tin >> hmm >> nval;
+            knowns[pemp]=nval;
+            i++;
+        }**/
+        ifstream rin("config/eq.ini");
+        while(rin >> a.lhs){
+            a.val.clear();
+            rin >> pemp >> a.rhs >> temp;
+            for(i=0;i<temp;i++){
+                rin >> pemp;
+                a.val.push_back(pemp);
+            }
+            myeq.push_back(a);
+        }
+        i=0;
+        vector<int>dfltind;
+        while(i<myeq.size()){
+            int counter=0;
+            dfltind.clear();
+            for(j=0;j<myeq[i].val.size();j++){
+                if(knowns.find(myeq[i].val[j])==knowns.end()){
+                    counter++;
+                    temp=j;
+                }
+            }
+            j=temp;
+            if(counter==1){
+/**
+u = 0
+a = 10
+s = 125
+**/
+                nval=solve(myeq[i].lhs,myeq[i].rhs,knowns,myeq[i].val[j]);
+                knowns[myeq[i].val[j]]=nval;
+                pemp=myeq[i].val[j].substr(0,3);
+                if(pemp=="sin"){
+                    mval=asin(nval);
+                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
+                    knowns[pemp]=mval*180/PI;
+                    knowns["cos"+pemp]=cos(mval);
+                    knowns["tan"+pemp]=tan(mval);
+                }
+                else if(pemp=="cos"){
+                    mval=acos(nval);
+                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
+                    knowns[pemp]=mval*180.0/PI;
+                    knowns["sin"+pemp]=sin(mval);
+                    knowns["tan"+pemp]=tan(mval);
+                }
+                else if(pemp=="tan"){
+                    mval=atan(nval);
+                    pemp=myeq[i].val[j].substr(3,myeq[i].val[j].length()-3);
+                    knowns[pemp]=mval*180.0/PI;
+                    knowns["cos"+pemp]=cos(mval);
+                    knowns["sin"+pemp]=sin(mval);
+                }
+                i=0;
+            }
+            else i++;
+        }
+        if(knowns.find(unknown)!=knowns.end())cout << knowns[unknown] << endl;
+    }
+    else{
+        map<string,double>::iterator myit;
+        string unknown="x",pemp, hmm;
+        string val1,val2;
+        vector<Equation> myeq;
+        Equation a;
+        cout << "Number of knowns (for first object)=";
+        cin >> temp;
+        cout << "Write them one at a line\n";
+        for(i=0;i<temp;i++){
+            cin >> pemp;
+            cin >> hmm >> nval;
+            knowns3[pemp]=nval;
+            if(pemp.find("theta")!=string::npos){
+                nval*=(PI/180);
+                knowns3[pemp]=nval;
+                knowns3["sin"+pemp]=sin(nval);
+                knowns3["cos"+pemp]=cos(nval);
+                knowns3["tan"+pemp]=tan(nval);
+            }
+            pemp.erase(pemp.length()-1);
+            knowns1[pemp]=nval;
+            if(pemp.find("theta")!=string::npos){
+                nval*=(PI/180);
+                knowns1[pemp]=nval;
+                knowns1["sin"+pemp]=sin(nval);
+                knowns1["cos"+pemp]=cos(nval);
+                knowns1["tan"+pemp]=tan(nval);
+            }
+        }
+        cout << "Number of knowns (for second object)=";
+        cin >> temp;
+        cout << "Write them one at a line\n";
+        for(i=0;i<temp;i++){
+            cin >> pemp;
+            cin >> hmm >> nval;
+            knowns3[pemp]=nval;
+            if(pemp.find("theta")!=string::npos){
+                nval*=(PI/180);
+                knowns3[pemp]=nval;
+                knowns3["sin"+pemp]=sin(nval);
+                knowns3["cos"+pemp]=cos(nval);
+                knowns3["tan"+pemp]=tan(nval);
+            }
+            pemp.erase(pemp.length()-1);
+            knowns2[pemp]=nval;
+            if(pemp.find("theta")!=string::npos){
+                nval*=(PI/180);
+                knowns2[pemp]=nval;
+                knowns2["sin"+pemp]=sin(nval);
+                knowns2["cos"+pemp]=cos(nval);
+                knowns2["tan"+pemp]=tan(nval);
+            }
+        }
+        vector<Equation> alge;
+        cout << "Any other conditions?y/n\n";
+        cin >> b;
+        int NumOfCondition=0;
+        if(b=='y'||b=='Y'){
+            cout << "Number of conditions=";
+            cin >> temp;
+            NumOfCondition=temp;
+            cout << "Write them one at a line\nFromat: Equation <space> variable1 <space> variable2\n";
+            for(i=0;i<temp;i++){
+                cin >> pemp;
+                cin >> hmm >> hmm;
+                a.lhs=pemp;
+                a.rhs=hmm;
+                cin >> pemp >> hmm;
+                a.val.clear();
+                a.val.push_back(pemp);
+                a.val.push_back(hmm);
+                alge.push_back(a);
+            }
+        }
+        cout << "What to find=";
+        cin >> unknown;
+        /**ifstream tin("const.txt");
+        i=0;
+        while(tin >> pemp){
+            tin >> hmm >> nval;
+            knowns[pemp]=nval;
+            i++;
+        }**/
+        ifstream rin("config/eq.ini");
+        while(rin >> a.lhs){
+            a.val.clear();
+            rin >> pemp >> a.rhs >> temp;
+            for(i=0;i<temp;i++){
+                rin >> pemp;
+                a.val.push_back(pemp);
+            }
+            myeq.push_back(a);
+        }
+        ifstream zin("config/con-eq.ini");
+        while(zin >> a.lhs){
+            a.val.clear();
+            zin >> pemp >> a.rhs >> temp;
+            for(i=0;i<temp;i++){
+                zin >> pemp;
+                a.val.push_back(pemp);
+            }
+            alge.push_back(a);
+        }
+        i=0;
+        vector<int>dfltind;
+        while(i<myeq.size()){
+            int counter=0;
+            dfltind.clear();
+            for(j=0;j<myeq[i].val.size();j++){
+                if(knowns1.find(myeq[i].val[j])==knowns1.end()){
+                    counter++;
+                    temp=j;
+                }
+            }
+            j=temp;
+            if(counter==1){
+                nval=solve(myeq[i].lhs,myeq[i].rhs,knowns1,myeq[i].val[j]);
+                myeq[i].val[j]+="1";
+                knowns3[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,3);
+                myeq[i].val[j].erase(myeq[i].val[j].length()-1);
+                knowns1[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,1);
+                i=0;
+            }
+            else i++;
+        }
+        i=0;
+        while(i<myeq.size()){
+            int counter=0;
+            dfltind.clear();
+            for(j=0;j<myeq[i].val.size();j++){
+                if(knowns2.find(myeq[i].val[j])==knowns2.end()){
+                    counter++;
+                    temp=j;
+                }
+            }
+            j=temp;
+            if(counter==1){
+                nval=solve(myeq[i].lhs,myeq[i].rhs,knowns2,myeq[i].val[j]);
+                myeq[i].val[j]+="2";
+                knowns3[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,3);
+                myeq[i].val[j].erase(myeq[i].val[j].length()-1);
+                knowns2[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,2);
+                i=0;
+            }
+            else i++;
+        }
+        i=0;
+        while(i<alge.size()){
+            int counter=0;
+            dfltind.clear();
+            for(j=0;j<alge[i].val.size();j++){
+                if(knowns3.find(alge[i].val[j])==knowns3.end()){
+                    counter++;
+                    temp=j;
+                }
+            }
+            j=temp;
+            if(counter==1){
+                nval=solve(alge[i].lhs,alge[i].rhs,knowns3,alge[i].val[j]);
+                knowns3[alge[i].val[j]]=nval;
+                trig(alge[i].val[j],nval,3);
+                i=0;
+            }
+            else i++;
+        }
+        if(NumOfCondition==1){
+            i=1;
+            while(i<alge.size()){
+                int counter=0;
+                dfltind.clear();
+                for(j=0;j<alge[i].val.size();j++){
+                    if(knowns3.find(alge[i].val[j])==knowns3.end()){
+                        if(alge[i].val[j]==alge[0].val[0]||alge[i].val[j]==alge[0].val[1])counter++;
+                    }
+                }
+                if(counter==2){
+                    nval=solve(alge[0].lhs,alge[0].rhs,alge[i].lhs,alge[i].rhs,knowns3,alge[0].val[0], alge[0].val[1]);
+                    knowns3[alge[0].val[1]]=nval;
+                    trig(alge[0].val[1],nval,3);
+                    pemp=alge[0].val[1];
+                    nval=solve(alge[0].lhs,alge[0].rhs,knowns3,alge[0].val[0]);
+                    knowns3[alge[0].val[0]]=nval;
+                    trig(alge[0].val[0],nval,3);
+                }
+                else i++;
+            }
+        }
+        else if(NumOfCondition==2){
+            i=0;
+            /**Each pair of variables of a certain system must be present in at least one equation of eq.txt**/
+            pemp=alge[0].val[0].substr(0,alge[0].val[0].length()-1);
+            hmm=alge[1].val[0].substr(0,alge[1].val[0].length()-1);
+            val1=alge[0].val[1].substr(0,alge[0].val[0].length()-1);
+            val2=alge[1].val[1].substr(0,alge[1].val[0].length()-1);
+            while(i<myeq.size()){
+                int counter1=0,couter2=0;
+                dfltind.clear();
+                for(j=0;j<myeq[i].val.size();j++){
+                    if(myeq[i].val[j]==val1||myeq[i].val[j]==val2) counter1++;
+                    if(knowns1.find(myeq[i].val[j])==knowns1.end()) couter2++;
+                }
+                if(counter1==2&&couter2==2){
+                    temp=i;
+                    break;
+                }
+                else i++;
+            }
+            i=0;
+            while(i<myeq.size()){
+                int counter1=0,couter2=0;
+                dfltind.clear();
+                for(j=0;j<myeq[i].val.size();j++){
+                    if(myeq[i].val[j]==val1||myeq[i].val[j]==val2) counter1++;
+                    if(knowns2.find(myeq[i].val[j])==knowns2.end()) couter2++;
+                }
+                if(counter1==2&&couter2==2){
+                    nval=i;
+                    break;
+                }
+                else i++;
+            }
+            vector<double> indice=SOLVE(myeq[temp],myeq[nval],alge[0],alge[1],knowns1, knowns2,pemp,hmm,val1,val2);
+            pemp+="1";
+            hmm+="1";
+            val1+="2";
+            val2+="2";
+            knowns3[pemp]=indice[0];
+            knowns3[hmm]=indice[1];
+            knowns3[val1]=indice[2];
+            knowns3[val2]=indice[3];
+        }
+        for(myit=knowns3.begin();myit!=knowns3.end();myit++){
+            pemp=myit->first;
+            if(pemp[pemp.length()-1]=='1'){
+                pemp.erase(pemp.length()-1);
+                knowns1[pemp]=myit->second;
+                trig(pemp,nval,1);
+            }
+            else if(pemp[pemp.length()-1]=='2'){
+                pemp.erase(pemp.length()-1);
+                knowns2[pemp]=myit->second;
+                trig(pemp,nval,2);
+            }
+        }
+        i=0;
+        while(i<myeq.size()){
+            int counter=0;
+            dfltind.clear();
+            for(j=0;j<myeq[i].val.size();j++){
+                if(knowns1.find(myeq[i].val[j])==knowns1.end()){
+                    counter++;
+                    temp=j;
+                }
+            }
+            j=temp;
+            if(counter==1){
+                nval=solve(myeq[i].lhs,myeq[i].rhs,knowns1,myeq[i].val[j]);
+                myeq[i].val[j]+="1";
+                knowns3[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,3);
+                myeq[i].val[j].erase(myeq[i].val[j].length()-1);
+                knowns1[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,1);
+                i=0;
+            }
+            else i++;
+        }
+        i=0;
+        while(i<myeq.size()){
+            int counter=0;
+            dfltind.clear();
+            for(j=0;j<myeq[i].val.size();j++){
+                if(knowns2.find(myeq[i].val[j])==knowns2.end()){
+                    counter++;
+                    temp=j;
+                }
+            }
+            j=temp;
+            if(counter==1){
+                nval=solve(myeq[i].lhs,myeq[i].rhs,knowns2,myeq[i].val[j]);
+                myeq[i].val[j]+="2";
+                knowns3[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,3);
+                myeq[i].val[j].erase(myeq[i].val[j].length()-1);
+                knowns2[myeq[i].val[j]]=nval;
+                trig(myeq[i].val[j],nval,2);
+                i=0;
+            }
+            else i++;
+        }
+        if(knowns3.find(unknown)!=knowns3.end())cout << knowns3[unknown] << endl;
+    }
+    cout << "(N)ew\t(Q)uit\n";
+    cin >> b;
+    if(b=='n'||b=='N') goto xx;
+    else return 0;
+}
+/**
+m
+2
+m1 = 0.1
+Sh1 = 4200
+2
+m2 = 0.2
+Sh2 = 4200
+y
+2
+Q = Q Q1 Q2
+delta_Theta = 60-delta_Theta delta_Theta1 delta_Theta2
+delta_Theta1**/
